f2eae0c3566f04334cde5d78d319bacb7f8aab8d
moshecohen1 <moshecohen7575@gmail.com>
2020-07-19 20:35:21 +0300

    fix board join

--- routes/index.js
+++ routes/index.js
@@ -22,11 +22,12 @@ let unmatched;
 const winCordenation = ['XXX', 'OOO'];
 
 const isGameOver = (board) => {
+  console.log(board[0].join(''))
   // possible winning combination
   const boardOptions = [
-    board[0].join[''],
-    board[1].join[''],
-    board[2].join[''],
+    board[0].join(''),
+    board[1].join(''),
+    board[2].join(''),
     `${board[0][0]}${board[1][0]}${board[2][0]}`,
     `${board[0][1]}${board[1][1]}${board[2][1]}`,
     `${board[0][2]}${board[1][2]}${board[2][2]}`,
